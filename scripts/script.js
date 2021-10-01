fb3bb8250587805686026130e84654761f0cf1a7
Azamat <muratovichazamat@yandex.com>
2021-10-01 16:33:52 +0500

    all pages finished

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYXphbWF0c2hhbXMiLCJhIjoiY2t1ODZxZWV6MHlyODJ5cGE2enJheHcyOSJ9.sVyJ36vJGENQl8t5HQmtOw";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+});
